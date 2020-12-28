a5bd5c28f01f8b6c10a05d97c935ad1a6b9834e6
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-28 23:58:51 +0000

    remove status and componentDidMount from Dogs,js into parent component App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React, { Component }  from 'react';
 import './App.css';
 import Dogs from './components/Dogs'
+import DisplayDog from './components/DisplayDog'
+import dogs from './dogs-data'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+        dogs: []
+    }
+  }
+
+  // mocking a fetch request
+  componentDidMount() {
+    // fetch the dogs ("www.dogs.com")
+    //  .then(r=>r.json())
+    //  .then(dogs => this.setState({dogs: dogs}))
+    this.setState({
+        dogs: dogs
+    })
+  }
   render () {
     return (
       <div className="App">
-        <Dogs/>
+        <h1>For the Love of Shih Tzu</h1>
+        { this.state.dogs.length === 0 
+        ?
+          "LOADING..."
+        :
+          <div>
+            <DisplayDog dog={this.state.dogs[0]}/>
+            <Dogs dogs={this.state.dogs}/>
+          </div>
+        }
+        
       </div>
     );
   }
