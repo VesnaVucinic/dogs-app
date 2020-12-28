45a5b548e8c8ca8fcb463da1e234ce48484e359e
VesnaVucinic <vesna.vucinic68@gmail.com>
2020-12-28 23:54:25 +0000

    add status

---
+++ src/dogs-data.js
@@ -0,0 +1,27 @@
+const dogs = [
+{status: "undecided", url:  "https://i.pinimg.com/236x/65/d4/d0/65d4d05ccf7dc26d3dc95b103be9d7c1.jpg", name: "Beauty" },
+{status: "undecided", url: "https://i.pinimg.com/236x/2b/a4/3d/2ba43dd5e3115e19801ad342c5353311.jpg", name: "Sleepy"},
+{status: "undecided", url: "https://i.pinimg.com/236x/13/dc/b3/13dcb361d43970c7fe153f4c2c72f9b0.jpg", name: "Lina"},
+{status: "undecided", url: "https://i.pinimg.com/236x/37/7a/a4/377aa42e2543bf9f1b51818cd5ea4769.jpg", name: "Masha"},
+{status: "undecided", url: "https://i.pinimg.com/236x/75/99/0a/75990a238166ef5a9208c099ee2840f7.jpg", name: "Cuty"},
+{status: "undecided", url: "https://i.pinimg.com/236x/a0/c5/46/a0c5462bb52f2e49aa8b631220113b41.jpg", name: "Lulu"},
+{status: "undecided", url: "https://i.pinimg.com/236x/02/14/48/021448600c6110ea06b0ea90318cba6f.jpg", name: "Mini"},
+{status: "undecided", url: "https://i.pinimg.com/236x/26/96/94/2696943029118d46637af5de3dcd324e.jpg", name: "Eyes"},
+{status: "undecided", url: "https://i.pinimg.com/236x/3e/df/a3/3edfa34e64d2f2a5aa1f4e25eedf1475.jpg", name: "Pepe"},
+{status: "undecided", url: "https://i.pinimg.com/236x/ff/15/a6/ff15a6283f4937492dbb431f73548beb.jpg", name: "Bonny"},
+{status: "undecided", url: "https://i.pinimg.com/236x/60/50/de/6050de7839b21eaedb1af21c01bb2730.jpg", name: "Micky"},
+{status: "undecided", url: "https://i.pinimg.com/236x/12/a5/54/12a5546e15ddfea9568cb3034b17bb4f.jpg", name: "Back"},
+{status: "undecided", url: "https://i.pinimg.com/236x/ab/1c/40/ab1c40c9d8cfb1cad1a5921e3639d3ec.jpg", name: "Sweetie"},
+{status: "undecided", url: "https://i.pinimg.com/236x/b5/d5/cc/b5d5cc7ec06a361025d59519e93f59da.jpg", name: "Will"},
+{status: "undecided", url: "https://i.pinimg.com/236x/1a/6c/d4/1a6cd4046a1ca5fe239ad883011ba0a1.jpg", name: "Sonny"},
+{status: "undecided", url: "https://i.pinimg.com/236x/63/c3/84/63c38477bf9ca421245f01725ab2dabc.jpg", name: "Zara"},
+{status: "undecided", url: "https://i.pinimg.com/236x/82/5b/75/825b75fccbf46adabcbbc67667eb264c.jpg", name: "Jake"},
+{status: "undecided", url: "https://i.pinimg.com/236x/be/05/d9/be05d902afd5789a62c444161aa9ee59.jpg", name: "Princess"},
+{status: "undecided", url: "https://i.pinimg.com/236x/e7/a9/8d/e7a98d0f4651901278a1dc21e0afde77.jpg", name: "Love"},
+{status: "undecided", url: "https://i.pinimg.com/236x/20/55/75/205575b7737bc4d6385b661443d09028.jpg", name: "Nera"},
+{status: "undecided", url: "https://i.pinimg.com/236x/05/27/94/05279452d21ec515a60f065c875e4a9b.jpg", name: "Banny"},
+{status: "undecided", url: "https://i.pinimg.com/564x/da/fa/80/dafa80ed0dab22567c840617e02d440a.jpg", name: "Haven"},
+{status: "undecided", url: "https://i.pinimg.com/236x/c8/53/fd/c853fdb1f289f4c7db6e1623c93e6d47.jpg", name: "Baby"}
+]
+
+export default dogs
